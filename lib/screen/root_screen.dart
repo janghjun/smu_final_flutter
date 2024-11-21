8a3a678e69ce986067fd120465fa6fb69331c898
jooho2075 <parkjh2075@icloud.com>
2024-11-21 23:50:28 +0900

    24.11.21_지도 경로 표시

--- lib/screen/root_screen.dart
+++ lib/screen/root_screen.dart
@@ -5,8 +5,6 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:smp_final_project/screen/badge_screen.dart';
 import 'package:smp_final_project/screen/settings_screen.dart';
-import 'package:location/location.dart';
-import 'package:smp_final_project/screen/start_screen.dart';
 
 class RootScreen extends StatefulWidget {
   const RootScreen({Key? key}) : super(key: key);
@@ -19,12 +17,31 @@ class _RootScreenState extends State<RootScreen> {
   int _selectedIndex = 0;
   LatLng? _currentLatLng;
   late GoogleMapController _mapController;
+  List<LatLng> _routePoints = []; // 이동 경로를 저장할 리스트
+  StreamSubscription<Position>? _positionStreamSubscription;
 
   // 타이머 관련 변수
   bool _isButtonVisible = true;
   bool _isTimerRunning = false;
   int _seconds = 0; // 타이머 값을 초 단위로 관리
   late Timer _timer;
+
+  // 버튼 스타일 변수화
+  final buttonStyle = ElevatedButton.styleFrom(
+    minimumSize: const Size(150, 50),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+    padding: const EdgeInsets.all(16.0),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _loadInitialLocation();
+  }
+
+  Future<void> _loadInitialLocation() async {
+    await _getCurrentLocation();
+  }
 
   // 현재 위치 가져오기
   Future<void> _getCurrentLocation() async {
@@ -51,68 +68,88 @@ class _RootScreenState extends State<RootScreen> {
       _currentLatLng = LatLng(position.latitude, position.longitude);
     });
 
-    if (_mapController != null) {
+    if (_mapController != null && _currentLatLng != null) {
       _mapController.animateCamera(
         CameraUpdate.newLatLng(_currentLatLng!),
       );
     }
   }
 
-  @override
-  void initState() {
-    super.initState();
-    _getCurrentLocation();
+  // 경로 추적 시작
+  void _startTracking() {
+    _resetState();
+
+    // 위치 스트림 구독 시작
+    _positionStreamSubscription =
+        Geolocator.getPositionStream(locationSettings: const LocationSettings())
+            .listen((Position position) {
+          final LatLng newLatLng = LatLng(position.latitude, position.longitude);
+          setState(() {
+            _currentLatLng = newLatLng;
+            _routePoints.add(newLatLng); // 새 위치를 경로 리스트에 추가
+          });
+
+          // 카메라 이동
+          if (_mapController != null) {
+            _mapController.animateCamera(
+              CameraUpdate.newLatLng(newLatLng),
+            );
+          }
+        });
+
+    // 타이머 시작
+    _startTimer();
   }
 
-  // 네비게이션 탭 변경
-  void _onItemTapped(int index) {
+  void _resetState() {
     setState(() {
-      _selectedIndex = index;
+      _isButtonVisible = false;
+      _isTimerRunning = true;
+      _routePoints.clear(); // 이전 경로 초기화
+      _seconds = 0; // 타이머 초기화
     });
   }
 
   // 타이머 시작
   void _startTimer() {
-    setState(() {
-      _isButtonVisible = false; // 시작 버튼 숨기기
-      _isTimerRunning = true; // 타이머 실행 상태로 변경
-    });
-
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       setState(() {
-        _seconds++; // 초 증가
-
-        // 60초가 지나면 분으로 전환
-        if (_seconds >= 60) {
-          _seconds = 0; // 초 초기화
-        }
+        _seconds++;
       });
     });
   }
 
   // 타이머 멈추기
   void _stopTimer() {
+    _timer.cancel();
     setState(() {
-      _timer.cancel(); // 타이머 멈추기
-      _isTimerRunning = false; // 타이머 실행 상태 종료
+      _isTimerRunning = false;
     });
   }
 
-  // 홈으로 돌아가기 (되돌아가기 버튼)
+  // 타이머 멈추기 및 경로 추적 종료
+  void _stopTracking() {
+    _stopTimer();
+    _positionStreamSubscription?.cancel(); // 위치 스트림 구독 해제
+    setState(() {
+      _isButtonVisible = true;
+      // 멈춘 시간은 계속 화면에 보이도록 유지
+    });
+  }
+
+  // 홈으로 돌아가기
   void _goHome() {
+    _stopTimer();
+    _positionStreamSubscription?.cancel(); // 위치 스트림 구독 해제
     setState(() {
-      _seconds = 0; // 타이머 리셋
-      _isButtonVisible = true; // 시작 버튼 표시
-      _isTimerRunning = false; // 타이머 실행 상태 종료
+      _isButtonVisible = true;
+      _isTimerRunning = false;
+      _routePoints.clear(); // 이전 경로 초기화
+      _seconds = 0; // 타이머 값 초기화
     });
   }
 
-  @override
-  void dispose() {
-    _timer.cancel(); // 타이머 리소스 해제
-    super.dispose();
-  }
-
+  // 시간 포맷팅
   String _formatTime(int seconds) {
     int hours = seconds ~/ 3600;
     int minutes = (seconds % 3600) ~/ 60;
@@ -123,6 +160,20 @@ class _RootScreenState extends State<RootScreen> {
 
   String _padZero(int value) {
     return value.toString().padLeft(2, '0');
+  }
+
+  // 네비게이션 탭 변경
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    _positionStreamSubscription?.cancel();
+    super.dispose();
   }
 
   @override
@@ -153,14 +204,8 @@ class _RootScreenState extends State<RootScreen> {
             child: Center(
               child: _isButtonVisible
                   ? ElevatedButton(
-                onPressed: _startTimer,
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(150, 50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  padding: const EdgeInsets.all(16.0),
-                ),
+                onPressed: _startTracking,
+                style: buttonStyle,
                 child: const Icon(
                   Icons.play_arrow_outlined,
                   size: 30.0,
@@ -170,6 +215,7 @@ class _RootScreenState extends State<RootScreen> {
                   : Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  // 멈춘 시간은 계속 표시됨
                   Text(
                     _formatTime(_seconds),
                     style: const TextStyle(
@@ -177,32 +223,21 @@ class _RootScreenState extends State<RootScreen> {
                       color: Colors.black,
                     ),
                   ),
-                  _isTimerRunning
-                      ? ElevatedButton(
-                    onPressed: _stopTimer,
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(150, 50),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
+                  // 멈춤 버튼은 타이머가 실행 중일 때만 보이도록
+                  if (_isTimerRunning)
+                    ElevatedButton(
+                      onPressed: _stopTracking,
+                      style: buttonStyle,
+                      child: const Icon(
+                        Icons.stop,
+                        size: 25.0,
+                        color: Colors.black,
                       ),
-                      padding: const EdgeInsets.all(16.0),
                     ),
-                    child: const Icon(
-                      Icons.stop,
-                      size: 25.0,
-                      color: Colors.black,
-                    ),
-                  )
-                      : Container(),
+                  // 홈 버튼은 항상 표시
                   ElevatedButton(
                     onPressed: _goHome,
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(150, 50),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      padding: const EdgeInsets.all(16.0),
-                    ),
+                    style: buttonStyle,
                     child: const Icon(
                       Icons.arrow_back_ios,
                       size: 25.0,
