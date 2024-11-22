2d4b9496b6eb50b8c924686d48fe230a1060eeb4
jooho2075 <parkjh2075@icloud.com>
2024-11-22 15:48:25 +0900

    24.11.22_root screen 형준이 버전으로 변경

--- lib/screen/root_screen.dart
+++ lib/screen/root_screen.dart
@@ -1,10 +1,12 @@
 import 'dart:async'; // 타이머 관련
+import 'dart:math'; // 수학 함수 및 상수 사용
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:pedometer/pedometer.dart';
 import 'package:smp_final_project/screen/badge_screen.dart';
 import 'package:smp_final_project/screen/settings_screen.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class RootScreen extends StatefulWidget {
   const RootScreen({Key? key}) : super(key: key);
@@ -17,7 +19,10 @@ class _RootScreenState extends State<RootScreen> {
   int _selectedIndex = 0;
   LatLng? _currentLatLng;
   late GoogleMapController _mapController;
-  List<LatLng> _routePoints = []; // 이동 경로를 저장할 리스트
+
+  final List<LatLng> _routePoints = []; // 이동 경로를 저장할 리스트
+  final Set<Polyline> _polylines = {};
+
   StreamSubscription<Position>? _positionStreamSubscription;
 
   // 타이머 관련 변수
@@ -25,6 +30,12 @@ class _RootScreenState extends State<RootScreen> {
   bool _isTimerRunning = false;
   int _seconds = 0; // 타이머 값을 초 단위로 관리
   late Timer _timer;
+
+  int _stepCount = 0;
+  double _distance = 0.0;
+
+  // pedometer 스트림을 사용하여 걸음 수 추적
+  late StreamSubscription<StepCount?> _stepCountStreamSubscription;
 
   // 버튼 스타일 변수화
   final buttonStyle = ElevatedButton.styleFrom(
@@ -36,7 +47,25 @@ class _RootScreenState extends State<RootScreen> {
   @override
   void initState() {
     super.initState();
+    _getCurrentLocation();
     _loadInitialLocation();
+    _startPedometer();
+  }
+
+  void _startPedometer() {
+    _stepCountStreamSubscription =
+        Pedometer.stepCountStream.listen((StepCount stepCount) {
+          setState(() {
+            _stepCount = stepCount.steps; // StepCount 객체에서 steps 사용
+          });
+        });
+  }
+
+  // 네비게이션 탭 변경
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   Future<void> _loadInitialLocation() async {
@@ -64,15 +93,48 @@ class _RootScreenState extends State<RootScreen> {
     }
 
     final Position position = await Geolocator.getCurrentPosition();
+    _updateRoute(LatLng(position.latitude, position.longitude));
+  }
+
+  void _updateRoute(LatLng newPosition) {
     setState(() {
-      _currentLatLng = LatLng(position.latitude, position.longitude);
+      if (_currentLatLng != null) {
+        _distance += _calculateDistance(_currentLatLng!, newPosition);
+      }
+      _currentLatLng = newPosition;
+      _routePoints.add(newPosition);
+      _polylines.add(Polyline(
+        polylineId: const PolylineId('route'),
+        points: _routePoints,
+        color: Colors.blue,
+        width: 5,
+      ));
     });
 
-    if (_mapController != null && _currentLatLng != null) {
+    if (_mapController != null) {
       _mapController.animateCamera(
-        CameraUpdate.newLatLng(_currentLatLng!),
+        CameraUpdate.newLatLng(newPosition),
       );
     }
+  }
+
+  double _calculateDistance(LatLng start, LatLng end) {
+    const double earthRadius = 6731;
+    double latDiff = _degreeToRadian(end.latitude - start.latitude);
+    double lngDiff = _degreeToRadian(end.longitude - start.longitude);
+
+    double a = sin(latDiff / 2) * sin(latDiff / 2) +
+        cos(_degreeToRadian(start.latitude)) *
+            cos(_degreeToRadian(end.latitude)) *
+            sin(lngDiff / 2) *
+            sin(lngDiff / 2);
+
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    return earthRadius * c;
+  }
+
+  double _degreeToRadian(double degree) {
+    return degree * pi / 180;
   }
 
   // 경로 추적 시작
@@ -112,6 +174,11 @@ class _RootScreenState extends State<RootScreen> {
 
   // 타이머 시작
   void _startTimer() {
+    setState(() {
+      _isButtonVisible = false;
+      _isTimerRunning = true;
+    });
+
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       setState(() {
         _seconds++;
@@ -121,8 +188,8 @@ class _RootScreenState extends State<RootScreen> {
 
   // 타이머 멈추기
   void _stopTimer() {
-    _timer.cancel();
     setState(() {
+      _timer.cancel();
       _isTimerRunning = false;
     });
   }
@@ -138,15 +205,20 @@ class _RootScreenState extends State<RootScreen> {
   }
 
   // 홈으로 돌아가기
-  void _goHome() {
-    _stopTimer();
-    _positionStreamSubscription?.cancel(); // 위치 스트림 구독 해제
+  void _endSession() {
     setState(() {
-      _isButtonVisible = true;
+      _timer.cancel();
       _isTimerRunning = false;
-      _routePoints.clear(); // 이전 경로 초기화
-      _seconds = 0; // 타이머 값 초기화
+      _selectedIndex = 1; // 성과 화면으로 이동
     });
+  }
+
+  @override
+  void dispose() {
+    _stepCountStreamSubscription?.cancel();
+    _timer.cancel();
+    _positionStreamSubscription?.cancel();
+    super.dispose();
   }
 
   // 시간 포맷팅
@@ -162,126 +234,132 @@ class _RootScreenState extends State<RootScreen> {
     return value.toString().padLeft(2, '0');
   }
 
-  // 네비게이션 탭 변경
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    _positionStreamSubscription?.cancel();
-    super.dispose();
+  Widget _buildInfoCard(String title, String value) {
+    return Expanded(
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              Text(
+                value,
+                style: const TextStyle(fontSize: 20.0, color: Colors.blue),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     final List<Widget> _pages = [
       Column(
-        mainAxisAlignment: MainAxisAlignment.center, // 위젯들 중앙 정렬
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Expanded(
             flex: 4,
-            child: _currentLatLng == null
-                ? const Center(child: CircularProgressIndicator())
-                : GoogleMap(
-              initialCameraPosition: CameraPosition(
-                target: _currentLatLng!,
-                zoom: 16,
-              ),
-              onMapCreated: (GoogleMapController controller) {
-                _mapController = controller;
-              },
-              myLocationEnabled: true,
-              myLocationButtonEnabled: true,
+            child: Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: _currentLatLng == null
+                      ? const Center(child: CircularProgressIndicator())
+                      : GoogleMap(
+                    initialCameraPosition: CameraPosition(
+                      target: _currentLatLng!,
+                      zoom: 16,
+                    ),
+                    onMapCreated: (GoogleMapController controller) {
+                      _mapController = controller;
+                    },
+                    myLocationEnabled: true,
+                    myLocationButtonEnabled: true,
+                    polylines: _polylines,
+                  ),
+                ),
+              ],
             ),
           ),
           Expanded(
-            flex: 1,
-            child: Center(
-              child: _isButtonVisible
-                  ? ElevatedButton(
-                onPressed: _startTracking,
-                style: buttonStyle,
-                child: const Icon(
-                  Icons.play_arrow_outlined,
-                  size: 30.0,
-                  color: Colors.black,
+            flex: 2,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    _buildInfoCard('걸음 수', '$_stepCount'),
+                    _buildInfoCard('거리', '${_distance.toStringAsFixed(2)}km'),
+                    _buildInfoCard('시간', _formatTime(_seconds)),
+                  ],
                 ),
-              )
-                  : Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // 멈춘 시간은 계속 표시됨
-                  Text(
-                    _formatTime(_seconds),
-                    style: const TextStyle(
-                      fontSize: 24.0,
-                      color: Colors.black,
-                    ),
-                  ),
-                  // 멈춤 버튼은 타이머가 실행 중일 때만 보이도록
-                  if (_isTimerRunning)
-                    ElevatedButton(
-                      onPressed: _stopTracking,
-                      style: buttonStyle,
-                      child: const Icon(
-                        Icons.stop,
-                        size: 25.0,
-                        color: Colors.black,
+                const SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    if (_isButtonVisible)
+                      ElevatedButton(
+                        onPressed: _startTimer,
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(120.0, 50.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                        child: const Text('시작'),
+                      )
+                    else
+                      ElevatedButton(
+                        onPressed: _stopTimer,
+                        child: const Text('정지'),
                       ),
+                    ElevatedButton(
+                      onPressed: _endSession,
+                      child: const Text('종료'),
                     ),
-                  // 홈 버튼은 항상 표시
-                  ElevatedButton(
-                    onPressed: _goHome,
-                    style: buttonStyle,
-                    child: const Icon(
-                      Icons.arrow_back_ios,
-                      size: 25.0,
-                      color: Colors.black,
-                    ),
-                  ),
-                ],
-              ),
+                  ],
+                ),
+              ],
             ),
           ),
         ],
       ),
-      BadgeScreen(stepCount: 0),
-      const SettingsScreen(),
+      BadgeScreen(stepCount: _stepCount),
+      SettingsScreen(),
     ];
 
     return Scaffold(
-      appBar: _selectedIndex == 0
-          ? AppBar(
+      appBar: AppBar(
         title: const Text('홈 화면'),
-      )
-          : null,
-      body: SafeArea(
-        child: IndexedStack(
-          index: _selectedIndex,
-          children: _pages,
-        ),
       ),
+      body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
-        selectedItemColor: Colors.black,
-        items: const <BottomNavigationBarItem>[
+        items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+            icon: Icon(FontAwesomeIcons.home),
             label: '홈',
           ),
           BottomNavigationBarItem(
             icon: Icon(FontAwesomeIcons.trophy),
-            label: '성과',
+            label: '성공',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: '환경설정',
+            icon: Icon(FontAwesomeIcons.gear),
+            label: '설정',
           ),
         ],
       ),
